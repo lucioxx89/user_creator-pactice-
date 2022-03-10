f53cecaff2f248070a61139c72bd5d4e7b216e10
Luciano Colabraro <luciano.colabraro@gmail.com>
2022-03-10 18:18:23 +0100

    form works but value is not yet stored

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,21 +1,29 @@
 import React, { useState } from "react";
 
 function AddUser() {
+  const [username, setUsername] = useState("");
+
   const usernameChangeHandler = (event) => {
     console.log(event.target.value);
+    setUsername(event.target.value);
   };
   const ageChangeHandler = (event) => {
     console.log(event.target.value);
   };
 
+  const FormSubmitHandler = (event) => {
+    event.preventDefault();
+    setUsername("");
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={FormSubmitHandler}>
         <div>
           <label>Username</label>
           <input
             onChange={usernameChangeHandler}
             type="text"
+            value={username} //2 ways binding
             placeholder="Write your username"
           />
         </div>
@@ -28,9 +36,13 @@ function AddUser() {
           />
         </div>
         <div>
-          <button>Add User</button>
+          <button type="submit">Add User</button>
         </div>
       </form>
+
+      <div>
+        <p>{username}</p>
+      </div>
     </div>
   );
 }
